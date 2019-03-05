7f83391f08052a45f04b42d82ee63e1eb74037e7
obruck <oscar.bruck@helsinki.fi>
2019-03-05 08:45:28 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup, find_packages
+setup(name='Fast_WSI_Color_Norm',
+      version='1.0',
+      url="https://github.com/obruck/Fast_WSI_Color_Norm",
+      author="deepakanandece",
+      packages = find_packages()
+)
